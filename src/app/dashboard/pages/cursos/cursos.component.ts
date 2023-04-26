fefc210aa0cf8f4acd87e69fd2f87161750c73c0
gmartinez <gmartinezfragoso@gmail.com>
2023-04-26 02:07:47 -0600

    Starting Courses

--- src/app/dashboard/pages/cursos/cursos.component.ts
+++ src/app/dashboard/pages/cursos/cursos.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Curso } from './models';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatDialog } from '@angular/material/dialog';
+import { CursosService } from './services/cursos.service';
 
 @Component({
   selector: 'app-cursos',
